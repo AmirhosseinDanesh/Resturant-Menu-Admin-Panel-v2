9e91b3b336000e35ea24013a03bb63cbcb7ef41f
AmirDanesh <daneshahd78@gmail.com>
2023-03-01 21:54:59 +0330

    Add Register PAge

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,8 +1,32 @@
 import React from 'react'
 import Input from '../../Components/Forms/Input'
 import { NavLink } from "react-router-dom"
+import { requiredValidator, minValidator, maxValidator, emailValidator } from "../../Validator/rules";
+import Button from "../../Components/Forms/button"
+import { useForm } from '../../Hooks/useForm';
 import "./Register.css"
 export default function Register() {
+    const [formState, onInputHandler] = useForm(
+        {
+            username: {
+                value: "",
+                isValid: false,
+            },
+            password: {
+                value: "",
+                isValid: false,
+            },
+        },
+        false
+    );
+
+    console.log(formState);
+
+    const userLogin = (event) => {
+        event.preventDefault();
+        console.log("User Login");
+    };
+
     return (
         <>
             <div className="loginformbg col-12 d-flex justify-content-center">
@@ -13,18 +37,65 @@ export default function Register() {
                     <form>
                         <div className="form-group mb-3">
                             <label className='input-label' >نام کاربری</label>
-                            <Input type="text" className="form-control" placeholder='AmirDanesh' element="input" />
+                            <Input
+                                id="username"
+                                type="text"
+                                className="form-control"
+                                placeholder='amirdanesh'
+                                element="input"
+                                validations={[
+                                    requiredValidator(),
+                                    minValidator(8),
+                                    maxValidator(18),
+                                ]}
+                                onInputHandler={onInputHandler}
+                            />
                         </div>
                         <div className="form-group mb-3">
                             <label className='input-label' >آدرس ایمیل</label>
-                            <Input type="email" className="form-control" placeholder='example@gmail.com' element="input" />
+                            <Input
+                                id="email"
+                                type="email"
+                                className="form-control"
+                                placeholder='example@gmail.com'
+                                element="input"
+                                validations={[
+                                    requiredValidator(),
+                                    minValidator(8),
+                                    maxValidator(18),
+                                    emailValidator()
+                                ]}
+                                onInputHandler={onInputHandler}
+                            />
                         </div>
                         <div className="form-group mb-3">
                             <label className='input-label' placeholder='خود را وارد کنید'>رمز عبور</label>
-                            <Input type="password" className="form-control" placeholder='********' element="input" />
+                            <Input
+                                id="password"
+                                type="password"
+                                className="form-control"
+                                placeholder='********'
+                                element="input"
+                                validations={[
+                                    requiredValidator(),
+                                    minValidator(8),
+                                    maxValidator(18),
+                                ]}
+                                onInputHandler={onInputHandler}
+                            />
                         </div>
                         <div className="d-flex align-items-center justify-content-between ">
-                            <button type="submit" className="submitbtn-login btn btn-success fw-bold">ورود</button>
+                            <Button
+                                className={`submitbtn-login btn fw-bold ${formState.isFormValid
+                                    ? "btn-success"
+                                    : "btn-danger"
+                                    }`}
+                                type="submit"
+                                onClick={userLogin}
+                                disabled={!formState.isFormValid}
+                            >
+                                ورود
+                            </Button>
                             <NavLink to="/login" className="text-white text-decoration-none">
                                 <button type="submit" className="submitbtn-login btn btn-success">اگر حساب کاربری <span className='fw-bold'>دارید</span> کلیک کنید</button>
                             </NavLink>
