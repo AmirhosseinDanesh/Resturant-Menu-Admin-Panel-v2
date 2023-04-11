6167f5071dc9e5c0469ddd75a8e45cd2abb70a22
Amir Danesh <62671734+AmirhosseinDanesh@users.noreply.github.com>
2023-04-11 22:58:24 +0430

    try to fix login bug

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,85 +1,77 @@
-import React, { useContext, useEffect, useState } from 'react'
-import "./Login.css"
-import Data from "../../Data/Data"
-import Input from '../../Components/Forms/Input'
+import React, { useContext, useEffect } from 'react';
+import "./Login.css";
+import Data from "../../Data/Data";
+import Input from '../../Components/Forms/Input';
 import { requiredValidator, minValidator, maxValidator, emailValidator } from "../../Validator/rules";
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom";
 import { useForm } from '../../Hooks/useForm';
-import Button from "../../Components/Forms/button"
-import authContext from '../../Contexts/authContext'
-import swal from "sweetalert"
-import { useNavigate } from 'react-router-dom';
+import Button from "../../Components/Forms/button";
+import authContext from '../../Contexts/authContext';
+import swal from "sweetalert";
 
 export default function Login() {
-    const auth = useContext(authContext)
-    const navigate = useNavigate()
-    const [isLoading, setIsLoading] = useState(false);
-
+    const auth = useContext(authContext);
+    const navigate = useNavigate();
     const [formState, onInputHandler] = useForm(
         {
             username: {
                 value: "",
-                isValid: true,
+                isValid: false,
             },
             password: {
                 value: "",
-                isValid: true,
+                isValid: false,
             },
         },
         false
     );
-
-
-    const userLogin = (event) => {
-        event.preventDefault();
-
-        const userData = {
-            identifier: formState.inputs.username.value,
-            password: formState.inputs.password.value,
-        }
-        setIsLoading(true);
-        fetch(`${Data.url}/auth/login`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(userData)
-        })
-            .then((res) => {
-                if (!res.ok) {
-                    return res.text().then((text) => {
-                        throw new Error(text);
-                    });
-                } else {
-                    return res.json();
-                }
-            })
-            .then((result) => {
-                swal({
-                    title: "با موفقیت لاگین شدید",
-                    icon: "success",
-                    buttons: "ورود به پنل",
-                }).then(() => {
-                    auth.login({}, result.accessToken);
-                    navigate("/p-admin/");
-
-                });
-            })
-            .catch((err) => {
-                swal({
-                    title: "همچین کاربری وجود ندارد",
-                    icon: "error",
-                    buttons: "تلاش دوباره",
-                })
-            })
-            .finally(() => setIsLoading(false));
-    }
 
     useEffect(() => {
         if (auth.isLoggedIn) {
             navigate("/p-admin/");
         }
     }, [auth.isLoggedIn, navigate]);
+
+    const userLogin = async (event) => {
+        event.preventDefault();
+
+        const userData = {
+            identifier: formState.inputs.username.value,
+            password: formState.inputs.password.value,
+        }
+
+        try {
+            const res = await fetch(`${Data.url}/auth/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(userData)
+            });
+
+            if (!res.ok) {
+                throw new Error('همچین کاربری وجود ندارد');
+            }
+
+            const result = await res.json();
+
+            swal({
+                title: "با موفقیت لاگین شدید",
+                icon: "success",
+                buttons: "ورود به پنل",
+            }).then(() => {
+                auth.login({}, result.accessToken);
+                navigate("/p-admin/");
+            });
+
+        } catch (err) {
+            swal({
+                title: err.message,
+                icon: "error",
+                buttons: "تلاش دوباره",
+            })
+        }
+    };
 
     return (
         <>
@@ -88,7 +80,7 @@ export default function Login() {
                     <h5 className='text-center mb-5'>
                         ورود
                     </h5>
-                    <form>
+                    <form onSubmit={userLogin}>
                         <div className="form-group mb-3">
                             <label className='input-label'>ایمیل یا نام کابری</label>
                             <Input
@@ -132,18 +124,18 @@ export default function Login() {
                                     : "btn-danger"
                                     }`}
                                 type="submit"
-                                onClick={userLogin}
+                                disabled={!formState.isFormValid}
                             >
                                 ورود
                             </Button>
 
                             <NavLink to="/register" className="text-white text-decoration-none">
-                                <button type="submit" className="submitbtn-login btn btn-danger">اگر حساب کاربری <span className='fw-bold'>ندارید</span> کلیک کنید</button>
+                                <button type="button" className="submitbtn-login btn btn-danger">اگر حساب کاربری <span className='fw-bold'>ندارید</span> کلیک کنید</button>
                             </NavLink>
                         </div>
                     </form>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
